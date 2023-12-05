a47c0488a6339b5233cf91e7e8e7238681d5cd52
maggie <maggi.noddle.is@gmail.com>
2023-12-05 16:38:15 +1100

    Initial commit for existing data and IDE set up for server

---
+++ src/existingData/staff.js
@@ -0,0 +1,64 @@
+const staff = [
+    {
+        id: 1,
+        name: 'Johh Smith',
+        phone: '02 9988 2211',
+        deptID: 1,
+        deptName: 'Information Communications Technology',
+        street: '1 Code Lane', 
+        city: 'Javaville',
+        state: 'NSW',
+        zip: '0100',
+        country: 'Australia'
+    },
+    {
+        id: 2,
+        name: 'Sue White',
+        phone: '03 8899 2255',
+        deptID: 2,
+        deptName: 'Finance',
+        street: '16 Bit Way', 
+        city: 'Byte Cove',
+        state: 'QLD',
+        zip: '1101',
+        country: 'Australia'
+    },
+    {
+        id: 3,
+        name: 'Bob O`Bits',
+        phone: '05 7788 2255',
+        deptID: 3,
+        deptName: 'Marketing',
+        street: '8 Silicon Road', 
+        city: 'Cloud Hills',
+        state: 'VIC',
+        zip: '1001',
+        country: 'Australia'
+    },
+    {
+        id: 4,
+        name: 'Mary Blue',
+        phone: '06 4455 9988',
+        deptID: 2,
+        deptName: 'Finance',
+        street: '4 Processor Boulevard', 
+        city: 'Appleston',
+        state: 'NT',
+        zip: '1010',
+        country: 'Australia'
+    },
+    {
+        id: 5,
+        name: 'Mick Green',
+        phone: '02 9988 1122',
+        deptID: 3,
+        deptName: 'Marketing',
+        street: '700 Bandwidh Street', 
+        city: 'Bufferland',
+        state: 'NSW',
+        zip: '0110',
+        country: 'Australia'
+    },
+];
+
+module.exports = staff;
